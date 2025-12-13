18a3aa0030cfe3b21002fa45c49b7f5ee95ca443
BRENNER Quentin <brenner.quentin@gmail.com>
2025-12-13 16:41:18 +0100

    feat: Added detailed documentation

--- minishell/include/executor.h
+++ minishell/include/executor.h
@@ -4,8 +4,26 @@
 #include "typedef.h"
 #include "internal_commands.h"
 
-int execute_command(Command* command);
-void execute_commands(Commands* commands);
-int create_child_process(Command* command);
+/**
+ * @brief Executes a single command, checking for internal commands first.
+ * @param command Pointer to the Command struct to execute.
+ * @param is_background Boolean indicating if the command should run in the background.
+ * @return Exit status of the command execution.
+ */
+int execute_command(Command* command, bool is_background);
+
+/**
+ * @brief Executes a series of commands with their associated operators.
+ * @param commands Pointer to the Commands struct containing commands and operators.
+ */
+void execute_commands(Commands* commands, BackgroundProcess* bg_processes);
+
+/**
+ * @brief Creates a child process to execute a command.
+ * @param command Pointer to the Command struct to execute.
+ * @param is_background Boolean indicating if the command should run in the background.
+ * @return Exit status of the command execution.
+ */
+int create_child_process(Command* command, bool is_background);
 
 #endif
--- minishell/include/internal_commands.h
+++ minishell/include/internal_commands.h
@@ -5,13 +5,48 @@
 
 extern char *work_dir;
 
+/**
+ * @brief Built-in cd command handler.
+ * @param args Command arguments, handles correctly changing directories, and specific cases like "cd ~". 
+ */
 void handle_cd(char** args);
+
+/**
+ * @brief Built-in pwd command handler.
+ * @param args voided   
+ */
 void handle_pwd(char** args);
+
+/**
+ * @brief Built-in echo command handler. 
+ * @param args Command arguments to echo back to the user.
+ */
 void handle_echo(char** args);
+
+/**
+ * @brief Built-in history command handler.
+ * Handles displaying the command history to the user.
+ * @param args voided
+ */
 void handle_history(char** args);
+
+/**
+ * @brief Built-in exit command handler.
+ * Exits the shell after freeing necessary resources.
+ * @param commands Pointer to the Commands struct containing commands and operators.
+ */
 void handle_exit(Commands* commands);
 
+/**
+ * @brief Frees memory if the pointer is not NULL.
+ * @param to_free Pointer to the memory to free.
+ */
 void free_if_needed(void* to_free);
+
+/**
+ * @brief Frees all allocated memory in the Commands struct.
+ * @param commands Pointer to the Commands struct to free.
+ */
 void free_commands(Commands* commands);
 
 #endif // INTERNAL_COMMANDS_H
